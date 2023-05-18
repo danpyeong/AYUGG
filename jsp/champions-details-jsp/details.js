b9f284854cc9b3e39791a2757e11cee6a82e9564
danpyeong <ujnzxc@gmail.com>
2023-05-18 17:19:43 +0900

    test 230518-1719

--- jsp/champions-details-jsp/details.js
+++ jsp/champions-details-jsp/details.js
@@ -1,3 +1,38 @@
+const axios = require("axios");
+
+window.onload = function () {
+  axios
+    .get(
+      "https://ddragon.leagueoflegends.com/cdn/11.22.1/data/ko_KR/champion.json"
+    )
+    .then(function (response) {
+      var championsData = response.data.data;
+      var championsDiv = document.getElementById("champions");
+
+      for (var championKey in championsData) {
+        if (championsData.hasOwnProperty(championKey)) {
+          var champion = championsData[championKey];
+          var championImage = champion.image;
+
+          var img = document.createElement("img");
+          img.src =
+            "https://ddragon.leagueoflegends.com/cdn/11.22.1/img/champion/" +
+            championImage.full;
+          img.width = 24;
+          img.height = 24;
+
+          var li = document.createElement("li");
+          li.appendChild(img);
+
+          championsDiv.appendChild(li);
+        }
+      }
+    })
+    .catch(function (error) {
+      console.error("API 요청 오류 :", error);
+    });
+};
+
 // 1 룬 세팅 선택에 따른 배경 변화
 var rune = document.getElementById("rune-1");
 rune.addEventListener("click", function (e) {
