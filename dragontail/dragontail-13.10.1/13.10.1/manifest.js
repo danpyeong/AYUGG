47962b5bb61834c7c34235ef2784e52f2dd63e2b
danpyeong <ujnzxc@gmail.com>
2023-05-18 18:07:39 +0900

    test

--- dragontail/dragontail-13.10.1/13.10.1/manifest.js
+++
@@ -1,1 +0,0 @@
-Riot.DDragon.m={"n":{"item":"13.10.1","rune":"13.10.1","mastery":"13.10.1","summoner":"13.10.1","champion":"13.10.1","profileicon":"13.10.1","map":"13.10.1","language":"13.10.1","sticker":"13.10.1"},"v":"13.10.1","l":"en_US","cdn":null,"dd":"13.10.1","lg":"13.10.1","css":"13.10.1","profileiconmax":28};
--- test.js
+++ test.js
@@ -1,3 +1,8 @@
 import axios from "axios";
+import jsonFile from "/Program Files/";
 
 var API_KEY = "RGAPI-df8af533-5da6-4e36-b93f-f5ca466ee8fb";
+
+<script src="/d" type="text/javascript"></script>;
+
+window.onload = function () {};
