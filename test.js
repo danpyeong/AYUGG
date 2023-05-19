09fb1874165be3d35e530ee6de2e889f266756d2
danpyeong <ujnzxc@gmail.com>
2023-05-20 04:04:13 +0900

    logo 추가 230520-0404

--- test.js
+++ test.js
@@ -1,8 +1,27 @@
 import axios from "axios";
-import jsonFile from "/Program Files/";
 
-var API_KEY = "RGAPI-df8af533-5da6-4e36-b93f-f5ca466ee8fb";
+// JSON 파일을 가져오는 함수
+function fetchJSONFile(url, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.overrideMimeType("application/json");
+  xhr.open("GET", url, true);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      callback(xhr.responseText);
+    }
+  };
+  xhr.send(null);
+}
 
-<script src="/d" type="text/javascript"></script>;
+// JSON 파일 가져오기
+fetchJSONFile(
+  "../dragontail/dragontail-13.10.1/13.10.1/data/ko_KR/championFull.json",
+  function (response) {
+    var jsonData = JSON.parse(response);
 
-window.onload = function () {};
+    // 가져온 JSON 데이터를 활용하여 작업 수행
+    // 예: jsonData 객체를 반복하거나 특정 필드에 접근
+
+    console.log(jsonData);
+  }
+);
