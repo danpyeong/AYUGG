958f559c3a7356ed1c5067da63a5c35721797662
danpyeong <ujnzxc@gmail.com>
2023-05-25 17:31:36 +0900

    챔피언 사진 넣기 230525-1731

--- test.js
+++ test.js
@@ -1,27 +1,43 @@
-import axios from "axios";
+const version = "https://ddragon.leagueoflegends.com/cdn/13.10.1/";
 
-// JSON 파일을 가져오는 함수
-function fetchJSONFile(url, callback) {
-  var xhr = new XMLHttpRequest();
-  xhr.overrideMimeType("application/json");
-  xhr.open("GET", url, true);
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      callback(xhr.responseText);
+const championUrl = version + "data/ko_KR/champion.json";
+
+fetch(championUrl)
+  .then((response) => response.json())
+  .then(function (data) {
+    // 모든 챔피언 데이터
+    var champData = data.data;
+
+    // 위 데이터를 담은 배열
+    var champDataList = Object.values(champData);
+    var champList = document.getElementById("champions-list");
+
+    // 한글 기준 오름차순 정렬
+    champDataList.sort(function (a, b) {
+      var nameA = a.name;
+      var nameB = b.name;
+      return nameA.localeCompare(nameB);
+    });
+
+    for (var i = 0; i < champDataList.length; i++) {
+      // console.log(champDataList[i]);
+
+      const childLi = document.createElement("li");
+      const childSpan = document.createElement("span");
+      const childImg = document.createElement("img");
+      const childDiv = document.createElement("div");
+
+      // 챔피언 초상화
+      childImg.src = version + "img/champion/" + champDataList[i].image.full;
+      childImg.width = 24;
+      childImg.height = 24;
+
+      // 챔피언 이름
+      childDiv.textContent = champDataList[i].name;
+
+      childSpan.appendChild(childImg);
+      childSpan.appendChild(childDiv);
+      childLi.appendChild(childSpan);
+      champList.appendChild(childLi);
     }
-  };
-  xhr.send(null);
-}
-
-// JSON 파일 가져오기
-fetchJSONFile(
-  "../dragontail/dragontail-13.10.1/13.10.1/data/ko_KR/championFull.json",
-  function (response) {
-    var jsonData = JSON.parse(response);
-
-    // 가져온 JSON 데이터를 활용하여 작업 수행
-    // 예: jsonData 객체를 반복하거나 특정 필드에 접근
-
-    console.log(jsonData);
-  }
-);
+  });
